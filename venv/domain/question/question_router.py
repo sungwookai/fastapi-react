b658dcbe2b0ae9a15f00816c0217d87ff49e9f45
sungwookai <sungwook.ai@gmail.com>
2023-07-17 17:27:28 +0900

    first commit

---
+++ venv/domain/question/question_router.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from database import get_db
+from domain.question import question_schema , question_crud
+from models import Question
+from typing import List
+
+router = APIRouter(
+    prefix="/api/question",
+)
+
+
+@router.get("/list", response_model=List[question_schema.Question])
+def question_list(db: Session = Depends(get_db)):
+    _question_list = question_crud.get_question_list(db)
+    return _question_list
