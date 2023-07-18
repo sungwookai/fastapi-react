41178e47f4e31dd473d8b72221c7ff46b83d7b9d
sungwookai <sungwook.ai@gmail.com>
2023-07-18 16:47:05 +0900

    second commit

--- venv/domain/question/question_crud.py
+++ venv/domain/question/question_crud.py
@@ -1,7 +1,11 @@
 from models import Question
 from sqlalchemy.orm import Session
 
-def get_question_list(db: Session):
+# create
+# read
+# update
+# delete
+def get_question_list(db: Session): ## read
     question_list = db.query(Question)\
         .order_by(Question.create_date.desc())\
         .all()
